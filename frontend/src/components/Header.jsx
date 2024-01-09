1e6da4439e5ef1b4650b30ddc1699eb5fc0c7319
Adrian <ad.dever@yahoo.com>
2024-01-09 17:32:16 +0100

    Updated logic/Added Database

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,5 +1,6 @@
 import { Navbar, Nav, Container, NavbarCollapse } from 'react-bootstrap'
 import { FaShoppingCart, FaUser } from 'react-icons/fa'
+import { LinkContainer } from'react-router-bootstrap'
 import logo from '../assets/logo.png'
 
 import React from 'react'
@@ -9,15 +10,21 @@ const Header = () => {
     <header>
         <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
             <Container>
-                <Navbar.Brand href="/">
+                <LinkContainer to='/'>
+                    <Navbar.Brand>
                     <img src={logo} alt="Proshop" />
                     Proshop
                     </Navbar.Brand>
+                </LinkContainer>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <NavbarCollapse id='basic-navbar-nav'>
                     <Nav className='ms-auto'>
-                        <Nav.Link href="/cart"><FaShoppingCart />Cart</Nav.Link>
-                        <Nav.Link href="/login"><FaUser />Login</Nav.Link>
+                        <LinkContainer to='/'>
+                        <Nav.Link><FaShoppingCart />Cart</Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to='/login'>
+                        <Nav.Link><FaUser />Login</Nav.Link>
+                        </LinkContainer>
                     </Nav>
                 </NavbarCollapse>
             </Container>
