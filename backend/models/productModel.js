64be9ce5c6ba720c72e4f55fca738a9047eb11f9
Adrian <ad.dever@yahoo.com>
2024-01-12 09:37:09 +0100

    added database seeder

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-reviewSchema = new mongoose.Schema(
+const reviewSchema = new mongoose.Schema(
   {
     user: {
       type: mongoose.Schema.Types.ObjectId,
@@ -48,7 +48,7 @@ const productSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
-    desciption: {
+    description: {
       type: String,
       required: true,
     },
